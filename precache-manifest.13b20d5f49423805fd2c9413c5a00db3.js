ac7cdb2902f9d770387f0433b44d9f5bfb13e909
rmn <rmnbrtshk@gmail.com>
2019-09-29 22:57:35 +0300

    Updates

---
+++ precache-manifest.13b20d5f49423805fd2c9413c5a00db3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de7fc60ec621aae43c8ffc3d895520c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4972f39744b2910f5bf",
+    "url": "./static/css/main.4ed7d7c5.chunk.css"
+  },
+  {
+    "revision": "413376f3a32206acde87",
+    "url": "./static/js/2.817dadb2.chunk.js"
+  },
+  {
+    "revision": "e4972f39744b2910f5bf",
+    "url": "./static/js/main.9042cad6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
